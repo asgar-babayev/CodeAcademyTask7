ba16ca91b0e93b29aee5b010a0464d238c32cc76
[Asgar Babayev] <[tu7609td3@code.edu.az]>
2022-03-15 05:20:12 +0400

    added

---
+++ ClassTask3/Models/Weapon.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassTask3.Models
+{
+    class Weapon
+    {
+        public int bulletCapacity;
+        public int bulletCount;
+        public double bulletShootSecond;
+        public bool autoMode;
+
+
+        public Weapon(int bulletCapacity, int bulletCount, double bulletShootSecond, bool autoMode)
+        {
+            this.bulletCapacity = bulletCapacity;
+            this.bulletCount = bulletCount;
+            this.bulletShootSecond = bulletShootSecond;
+            this.autoMode = autoMode;
+            if (bulletCapacity < bulletCount)
+            {
+                Console.WriteLine("Güllə sayısı güllə tutumundan çox ola bilməz");
+            }
+        }
+
+        public void Shoot()
+        {
+            if (autoMode == true)
+                Console.WriteLine("Auto modda tək güllə atıla bilməz");
+            else if (bulletCount != 0)
+            {
+                Console.WriteLine("pew");
+                Console.WriteLine($"Qalan güllə sayısı: {--bulletCount}");
+            }
+            else
+                Console.WriteLine("Daraq boşdur");
+        }
+
+        public void Fire()
+        {
+            double shootedbulletSecond = 0;
+            if (bulletCount != 0)
+            {
+                shootedbulletSecond = (bulletCount * bulletShootSecond) / bulletCapacity;
+                bulletCount = 0;
+                Console.WriteLine($"Güllə {Math.Round(shootedbulletSecond, 2)} saniyəyə bitdi");
+                if (autoMode == true)
+                    Console.WriteLine("pew pew");
+                else
+                    Console.WriteLine("pew");
+            }
+            else
+                Console.WriteLine("Daraq boşdur");
+        }
+
+        public void GetRemainBulletCount()
+        {
+            if (bulletCount != 0 && bulletCapacity != 0 && bulletCapacity < bulletCount)
+            {
+                if (bulletCapacity - bulletCount == 0)
+                    Console.WriteLine("Daraq doludur.");
+                else
+                    Console.WriteLine($"Darağın dolması ücün lazım olan güllə sayı = {bulletCapacity - bulletCount}");
+            }
+            else
+                Console.WriteLine("Bu işdə bir tərslik var");
+        }
+
+        public void Reload()
+        {
+            if (bulletCapacity != 0 && bulletCapacity > bulletCount)
+            {
+                if (bulletCapacity - bulletCount == 0)
+                    Console.WriteLine("Daraq doludur");
+                else if(bulletCount == 0)
+                {
+                    Console.WriteLine($"Daraq maksimum dolduruldu. Güllə sayı: {bulletCapacity - bulletCount}");
+                    bulletCount = bulletCapacity;
+                }
+            }
+            else
+                Console.WriteLine("Bu işdə bir tərslik var");
+        }
+        public void ChangeFireMode()
+        {
+            if (autoMode == false)
+            {
+                Console.WriteLine("Avtomatik mod aktivdir");
+                autoMode = true;
+            }
+            else
+            {
+                Console.WriteLine("Avtomatik mod passivdir");
+                autoMode = false;
+            }
+        }
+        public void Info()
+        {
+            Console.WriteLine(@$"Maksimum tutum: {bulletCapacity}
+Hazırki güllə sayısı: {bulletCount}
+Güllənin boşalma saniyəsi :{bulletShootSecond}
+Güllənin atış modu: {autoMode}
+");
+        }
+
+    }
+
+}
---
+++ ClassTask3/Program.cs
@@ -0,0 +1,71 @@
+﻿using ClassTask3.Models;
+using System;
+
+namespace ClassTask3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.GetEncoding("UTF-16");
+
+
+            Console.Write("Darağın maksimum tutumunu daxil edin: ");
+            int bulletCapacity = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Hazırda olan güllə sayını daxil edin: ");
+            int bulletCount = Convert.ToInt32(Console.ReadLine()); ;
+            Console.Write("Darağın boşalma saniyəsini daxil edin: ");
+            double bulletShootSecond = Convert.ToDouble(Console.ReadLine()); ;
+            Console.Write("Avtomatik modun aktiv və ya passiv olmasını daxil edin(True ya da False): ");
+            bool autoMode = Convert.ToBoolean(Console.ReadLine()); ;
+            Weapon w = new Weapon(bulletCapacity, bulletCount, bulletShootSecond, autoMode);
+
+            int key = 0;
+            while (key != 6)
+            {
+                Console.WriteLine(@$"0-Info:
+1-Shoot
+2-Fire
+3-GetRemainBulletCount
+4-Reload
+5-ChangeFireMode
+6-Sagolun
+");
+                key = Convert.ToInt32(Console.ReadLine());
+                switch (key)
+                {
+                    case 0:
+                        Console.WriteLine("Ümümi Məlumat");
+                        w.Info();
+                        break;
+                    case 1:
+                        Console.WriteLine("Shoot metodu cagirildi");
+                        w.Shoot();
+                        break;
+                    case 2:
+                        Console.WriteLine("Fire metodu cagirildi");
+                        w.Fire();
+                        break;
+                    case 3:
+                        Console.WriteLine("GetRemainBulletCount metodu cagirildi");
+                        w.GetRemainBulletCount();
+                        break;
+                    case 4:
+                        Console.WriteLine("Reload metodu cagirildi");
+                        w.Reload();
+                        break;
+                    case 5:
+                        Console.WriteLine("ChangeFireMode metodu cagirildi");
+                        w.ChangeFireMode();
+                        break;
+                    case 6:
+                        Console.WriteLine("Sagolun.");
+                        break;
+                    default:
+                        Console.WriteLine("0-6 araliginda reqem daxil edib metodlari cagira bilersiz");
+                        break;
+                }
+            }
+        }
+    }
+}
